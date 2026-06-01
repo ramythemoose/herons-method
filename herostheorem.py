13f9e4160661baa165270e9f193939e9daa0d3a3
ramythemoose <mooseramy9982@gmail.com>
2026-06-01 19:12:01 +0100

    better-written code
    added functions for fixed xn, variable xn, fixed k and variable k for better plotting capabilities and user control
    
    added a table to see each convergence step and corresponding xn value in original heroes theorem
    
    added some text!

--- herostheorem.py
+++ herostheorem.py
@@ -1,45 +1,101 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import tabulate as tabulate
+import pandas as pd
+
+
+np.set_printoptions(legacy='1.25') #stops the annoying npfloat64 output for printing floats in a list
 
 def herostheorem(xn, k):
     results_list = [xn]
 
-    while True:
+    while xn != 0:
         x_nplus1 = (xn + k/xn)/2
         results_list.append(x_nplus1)
 
-        if round(x_nplus1, 5) == round(xn, 5):
+        if round(x_nplus1, 3) == round(xn, 3):
             break
         else:
             xn = x_nplus1
-            print(round(xn, 5))
+        if xn == 0: 
+            print(f"You cannot divide by zero!")
+            break #break algorithm if xn == 0
     
     n_list = list(range(len(results_list)))
+    
+    results_df = pd.DataFrame(n_list,
+                              results_list)
+    print(tabulate.tabulate(results_df, headers=("X_n","Convergence Step"), tablefmt="grid"))
+    #create a table for fixed xn and k values and show convergence steps
+    print(f"The square root of {k} is {results_list[-1]:.3f} to 3 decimal places.\n From x_0 = {results_list[0]}, it took {len(n_list)-1} steps to converge.")
+
     return n_list, results_list
 
-def plottingfunc(xn_range, xn_intervals, k_range, 
-                 k_intervals, xn_start=0, k_start=1, **kwargs):
-    xn = [i for i in np.linspace(xn_start, xn_range, xn_intervals) if  i != 0] 
-    #generates values for xn in linspace from xn_start to xn_range, xn_intervals
-    #if xn = 0, reject
-    k = [i for i in np.linspace(k_start, k_range, k_intervals) if i > 0]
+def plottingfunc(xn_range, k_range, xn_intervals,
+                 k_intervals, xn_start, k_start):
+    xn = [i for i in np.arange(xn_start, xn_range+1, xn_intervals) if  i != 0] 
+    #list of values for xn in linspace from xn_start to xn_range, xn_intervals. list comp because generators hate loops
+    #if xn = 0, reject for k/xn = undefined
+    k = [j for j in np.arange(k_start, k_range+1, k_intervals) if j > 0]
     #reject negative square roots
-
-    for num in xn:
-        for value in k:
-            x, y = herostheorem(num, value)
-            plt.plot (x, y)
     
-    plt.ylabel("x_n+1")
-    plt.xlabel("N convergences")
+    for i in xn: #loop over xn list
+        for j in k: #loop over k list
+            n_list, results_list = herostheorem(i, j) #calculate heros theorem for each value of xn over a range of k values
+            plt.plot(n_list, results_list) #plot convergence steps against values for xn+1
+            print(n_list, results_list)
+            print(i, j)
+            
+
+    interface_txt = f"Plotting: \nk: {k_start} -> {k_range} in steps of {k_intervals} \nxn: {xn_start} -> {xn_range} in steps of {xn_intervals}"
+    print(interface_txt, end='')
     plt.show()
 
-plottingfunc(xn_range=5, #loop from 0 to xn_range starting guess
-             xn_intervals=10, #number of points from 0 to xn_range using linspace
-             k_range=1, #loop from 0 to k for sqrt(k)
-             k_intervals=10, #number of points from 0 to k_range 
-             xn_start=2, #starting point of xn (opt)
-             k_start=-5) #starting point of k (opt)
+def plot_varxn_fixedk(xn_start, xn_range, xn_intervals, k):
+    fixedk_plot = plottingfunc(xn_range=xn_range, 
+            xn_intervals=xn_intervals, 
+            k_range=k, 
+            k_intervals=1, 
+            xn_start=xn_start, 
+            k_start=k) 
+    
+    return fixedk_plot #variable xn range, fixed k plot
+
+def plot_fixedxn_vark(k_start, k_range, k_intervals, xn):
+    fixedxn_plot = plottingfunc(xn_range=xn,
+            xn_intervals=1, 
+            k_range=k_range, 
+            k_intervals=k_intervals, 
+            xn_start=xn, 
+            k_start=k_start) 
+    
+    return fixedxn_plot #fixed xn, variable k plot
+
+def plot_varxn_vark(xn_start, xn_range, xn_intervals, k_start, k_range, k_intervals):
+    varall_plot = plottingfunc(xn_range=xn_range, 
+            xn_intervals=xn_intervals, 
+            k_range=k_range, 
+            k_intervals=k_intervals, 
+            xn_start=xn_start, 
+            k_start=k_start) 
+    
+    return varall_plot #variable xn range, variable k plot
+
+def plot_fixedxn_fixedk(xn, k):
+    fixedall_plot = plottingfunc(xn_range=xn, 
+            xn_intervals=1, 
+            k_range=k, 
+            k_intervals=k, 
+            xn_start=xn, 
+            k_start=k)
+    
+    return fixedall_plot #fixed xn, fixed k plot
+    
+#herostheorem(10.3, 99.9)
+#plot_varxn_fixedk(0, 10, 2, k=100)
+#plot_fixedxn_vark(0, 10, 2, xn=100)
+#plot_varxn_vark(0, 100, 10, 0, 100, 10)
+#plot_fixedxn_fixedk(8, 100)
 
 
 
